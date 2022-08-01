b1a6cef086bcf171d0736bf29dd4ef884f8e5081
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2022-08-02 00:13:56 +0500

    Add files via upload

---
+++ Assignment 1/assignmentOne.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.5.0;
+/*
+Ahmad Saad
+231461463
+Solidity Assignment 1
+*/
+
+contract assignmentOne {
+    uint256 public average;
+    uint256 public metricBMI;
+    uint256 public imperialBMI;
+    address public deployerAccount;
+
+    /*
+    Q1) Write a function which takes 5 numbers and returns their average.
+    */
+    function calculateAverage(uint One, uint Two, uint Three, uint Four, uint Five) public{
+        average = (One + Two + Three + Four + Five) / 5;
+    }
+
+
+    /* 
+    Q2) Write 2 functons to calculate BMI (Body-mass index) using 
+    both formulas (2 separate functions).
+    */
+
+    // Function 1 (Metric Units)
+    function metricBMIClaculator(uint weight, uint height) public{
+        metricBMI = weight / (height ** 2);
+    }
+
+    // Function 2 (Imperial system)
+    function imperialBMICalculator(uint weight, uint height) public{
+        imperialBMI = 703 * weight / (height ** 2);
+    }
+
+    /*
+    Q3) Write a function which returns the address of the account 
+    that deployed the contract.
+    */
+
+    // First we use a setter function to set the address
+    constructor() public{
+        deployerAccount = msg.sender;
+    }
+
+    // Then we retrieve the address
+    function getAddress() public view returns (address){    
+        return deployerAccount;
+    }
+}
---
+++ Assignment 1/questionFive.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.5.0;
+/*
+Ahmad Saad
+231461463
+Solidity Assignment 1
+Question 5
+*/
+
+contract questionFive{
+    /*
+    Create an escrow application where the user can deposit money to the 
+    smart contract, withdraw money from the smart contract to their own 
+    account and they should also be able to withdraw money to another 
+    account. Your program should also have helper function that displays 
+    the amount of money your smart contract has and also another function 
+    that displays the amount of money any address has.
+    */
+
+    function depositToContract() public payable{}
+
+    function getBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    function withdrawAmount() public payable{
+        address payable to = msg.sender;
+        to.transfer(this.getBalance());
+    }
+
+    function DepositAmount(address payable addressArg) public payable{
+        addressArg.transfer(this.getBalance());
+    }
+
+    function findBalance(address addressArgTwo) public view returns(uint){
+        return addressArgTwo.balance;
+    }
+}
