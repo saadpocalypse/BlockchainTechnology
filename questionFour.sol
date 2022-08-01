bc720040b62084a47ef962f33db0bbe93b313f8b
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2022-08-02 00:12:31 +0500

    Add files via upload

---
+++ questionFour.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.5.0;
+/*
+Ahmad Saad
+231461463
+Solidity Assignment 1
+Question 4
+*/
+
+contract questionFour{
+    uint256 public calcSum;
+    uint256 public calcDif;
+    uint256 public calcProd;
+    uint256 public calcDiv;
+
+    /*
+    Create a simple calculator which takes 2 numbers as inputs and then 
+    calculates addition, subtraction, multiplication, and division 
+    in different functions.
+    */
+
+    // Addition
+    function addition(uint inputOne, uint inputTwo) public{
+        calcSum = inputOne + inputTwo;
+    }
+
+    // Subtraction
+    function subtraction(uint inputOne, uint inputTwo) public{
+        calcDif =  inputOne - inputTwo;
+    }
+
+    // Multiplication
+    function multiplication(uint inputOne, uint inputTwo) public{
+        calcProd = inputOne * inputTwo;
+    }
+
+    // Division
+    function division(uint inputOne, uint inputTwo) public{
+        calcDiv =  inputOne / inputTwo;
+    }
+}
