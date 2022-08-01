fcc89853eb6df3f0a4b3446a4b79eafb927cc266
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2022-08-02 00:29:49 +0500

    Delete activityTwo.sol

--- Assignment 1/activityTwo.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-contract divisibilityChecker{
-    function twoAndFourDivisibility(uint num) public{
-        require(num % 4 == 0, "This number is not divisible by 4!");
-        require(num % 2 == 0, "This number is not divisible by 2 and 4!");
-        require(num % 2 != 0, "This number is divisible by 2 and 4!");
-    }
-}
