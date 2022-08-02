0bdae53743465bbb03cc85ff21bf77a6da307fb7
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2022-08-03 01:42:20 +0500

    Add files via upload

---
+++ Activity 1/labOne.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.0;
+
+contract labOne {
+    uint256 length;
+    uint256 width;
+    uint256 public Area;
+    uint256 public Perimeter;
+
+    function setLength(uint _length) public{
+        length = _length;
+    }
+
+    function setWidth(uint _width) public{
+        width = _width;
+    }
+
+    function setArea() public{
+        Area =  length * width;
+    }
+
+    function setPerimeter() public {
+        Perimeter =  (length + width) + (length + width);
+    }
+
+    function getArea() public view returns (uint) {
+        return Area;
+    }
+
+    function getPerimeter() public view returns(uint) {
+        return Perimeter;
+    }
+}
